aab8dc13f866724344a5e74343f81d50dc331f52
paisleyremovalsandupliftsltd-boop <paisleyremovalsandupliftsltd@gmail.com>
2026-03-26 23:46:56 +0000

    Update send-quote.js

--- api/send-quote.js
+++ api/send-quote.js
@@ -7,19 +7,43 @@ export default async function handler(req, res) {
     return res.status(405).end();
   }
 
-  const { name, email, phone, message } = req.body;
+  const {
+    name,
+    email,
+    phone,
+    service,
+    propertyType,
+    moveDate,
+    fromAddress,
+    toAddress,
+    message
+  } = req.body;
 
   try {
     await resend.emails.send({
       from: 'Paisley Removals <quotes@paisleyremovalsanduplift.co.uk>',
-      to: 'YOUR_EMAIL_HERE', // change this
+      to: 'YOUR_EMAIL_HERE',
       subject: 'New Quote Request',
       html: `
         <h2>New Quote Request</h2>
+
         <p><b>Name:</b> ${name}</p>
         <p><b>Email:</b> ${email}</p>
         <p><b>Phone:</b> ${phone}</p>
-        <p><b>Message:</b> ${message}</p>
+
+        <hr>
+
+        <p><b>Service:</b> ${service}</p>
+        <p><b>Property Type:</b> ${propertyType}</p>
+        <p><b>Move Date:</b> ${moveDate}</p>
+
+        <p><b>From Address:</b> ${fromAddress}</p>
+        <p><b>To Address:</b> ${toAddress}</p>
+
+        <hr>
+
+        <p><b>Additional Info:</b></p>
+        <p>${message}</p>
       `,
     });
 
